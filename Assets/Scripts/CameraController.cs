9c275b8e9174f89e785bdf7e9a4945ec6947b576
Josh Newland <joshnewland@josh-newlands-macbook.local>
2017-04-22 15:28:00 +0100

    Camera follow player, zoom & player movement
    Camera should follow the player.
    Camera should zoom in and out with the mouse wheel.
    WIP : Player should move in x & y with (WASD & arrow keys) in a totally
    non boat like fashion (TODO)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour 
+{
+	public Transform target;
+
+	private Vector3 _targetOffset = new Vector3(0,0,-10);
+	private float _moveSpeed = 12;
+
+	private float _minOrthSize = 5;
+	private float _maxOrthSize = 20;
+
+	private Camera _cam;
+	private float _scrollInput;
+	private float _camOrthSize;
+	private float _camZoomSpeed = 10;
+
+	void Start () 
+	{
+		_cam = GetComponent<Camera>();
+	}
+	
+	void Update () 
+	{
+		if (target != null)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, target.transform.position + _targetOffset, _moveSpeed * Time.deltaTime);
+		}
+
+		UpdateCameraZoom();
+	}
+
+	void UpdateCameraZoom()
+	{
+		_scrollInput = Input.GetAxis("Mouse ScrollWheel");
+		_camOrthSize = _cam.orthographicSize;
+		_camOrthSize += _scrollInput * Time.deltaTime * _camZoomSpeed;
+
+		if (_camOrthSize < _minOrthSize)
+		{
+			_camOrthSize = _minOrthSize;
+		}
+
+		if (_camOrthSize > _maxOrthSize)
+		{
+			_camOrthSize = _maxOrthSize;
+		}
+
+		_cam.orthographicSize = _camOrthSize;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	private float _xAxisInput = 0;
+	private float _yAxisInput = 0;
+
+	private float _currVelocity;
+
+	private float _moveSpeed = 3;
+	private float _maxSpeed = 10;
+
+	private Rigidbody2D _rb2d;
+
+	void Start () 
+	{
+		_rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate () 
+	{
+		UpdateMove();
+	}
+
+	void UpdateMove ()
+	{
+		_xAxisInput = Input.GetAxis("Horizontal");
+		_yAxisInput = Input.GetAxis("Vertical");
+
+		//TODO: Boat's do not work like this ... MAKE IT MORE BOATY !!! ...
+
+		_rb2d.AddForce( new Vector2(_xAxisInput, _yAxisInput) * _moveSpeed );
+
+		_currVelocity = _rb2d.velocity.magnitude;
+		if (_currVelocity > _maxSpeed)
+		{
+			float velocityDifNormal = _maxSpeed / _currVelocity;
+			_rb2d.velocity = new Vector2( _rb2d.velocity.x * velocityDifNormal, _rb2d.velocity.y * velocityDifNormal);
+		}
+	}
+}
